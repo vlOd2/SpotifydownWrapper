912db713db040873ec0e7575367315637292f7c7
vlOd <vlodiscord@gmail.com>
2023-03-02 22:05:52 +0200

    Add project files.

---
+++ SpotifydownWrapper/MainForm.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace SpotifydownWrapper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.txtPlaylistID = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.pbProgress = new System.Windows.Forms.ProgressBar();
+            this.lStatus = new System.Windows.Forms.Label();
+            this.lTracksDone = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tsMenuBar = new System.Windows.Forms.ToolStrip();
+            this.tsddbMenuBarHelp = new System.Windows.Forms.ToolStripDropDownButton();
+            this.tsmiMenuBarHelpAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.fbdBrowse = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtOutputFolderPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.pbStatus = new System.Windows.Forms.PictureBox();
+            this.tsMenuBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStatus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtPlaylistID
+            // 
+            this.txtPlaylistID.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPlaylistID.Location = new System.Drawing.Point(12, 55);
+            this.txtPlaylistID.Name = "txtPlaylistID";
+            this.txtPlaylistID.Size = new System.Drawing.Size(457, 20);
+            this.txtPlaylistID.TabIndex = 0;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDownload.ImageKey = "DownloadStaticEmpty.png";
+            this.btnDownload.Location = new System.Drawing.Point(471, 54);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 1;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // pbProgress
+            // 
+            this.pbProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbProgress.Location = new System.Drawing.Point(12, 122);
+            this.pbProgress.Name = "pbProgress";
+            this.pbProgress.Size = new System.Drawing.Size(534, 23);
+            this.pbProgress.TabIndex = 2;
+            // 
+            // lStatus
+            // 
+            this.lStatus.AutoSize = true;
+            this.lStatus.Location = new System.Drawing.Point(42, 153);
+            this.lStatus.Name = "lStatus";
+            this.lStatus.Size = new System.Drawing.Size(87, 13);
+            this.lStatus.TabIndex = 3;
+            this.lStatus.Text = "Not downloading";
+            // 
+            // lTracksDone
+            // 
+            this.lTracksDone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lTracksDone.Location = new System.Drawing.Point(471, 148);
+            this.lTracksDone.Name = "lTracksDone";
+            this.lTracksDone.Size = new System.Drawing.Size(75, 13);
+            this.lTracksDone.TabIndex = 4;
+            this.lTracksDone.Text = "0/0";
+            this.lTracksDone.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Playlist ID:";
+            // 
+            // tsMenuBar
+            // 
+            this.tsMenuBar.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.tsMenuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsddbMenuBarHelp});
+            this.tsMenuBar.Location = new System.Drawing.Point(0, 0);
+            this.tsMenuBar.Name = "tsMenuBar";
+            this.tsMenuBar.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.tsMenuBar.Size = new System.Drawing.Size(558, 25);
+            this.tsMenuBar.TabIndex = 6;
+            this.tsMenuBar.Text = "Menu bar";
+            // 
+            // tsddbMenuBarHelp
+            // 
+            this.tsddbMenuBarHelp.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsddbMenuBarHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiMenuBarHelpAbout});
+            this.tsddbMenuBarHelp.Image = ((System.Drawing.Image)(resources.GetObject("tsddbMenuBarHelp.Image")));
+            this.tsddbMenuBarHelp.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsddbMenuBarHelp.Name = "tsddbMenuBarHelp";
+            this.tsddbMenuBarHelp.ShowDropDownArrow = false;
+            this.tsddbMenuBarHelp.Size = new System.Drawing.Size(36, 22);
+            this.tsddbMenuBarHelp.Text = "Help";
+            this.tsddbMenuBarHelp.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // tsmiMenuBarHelpAbout
+            // 
+            this.tsmiMenuBarHelpAbout.Name = "tsmiMenuBarHelpAbout";
+            this.tsmiMenuBarHelpAbout.Size = new System.Drawing.Size(107, 22);
+            this.tsmiMenuBarHelpAbout.Text = "About";
+            this.tsmiMenuBarHelpAbout.Click += new System.EventHandler(this.tsmiMenuBarHelpAbout_Click);
+            // 
+            // txtOutputFolderPath
+            // 
+            this.txtOutputFolderPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutputFolderPath.Location = new System.Drawing.Point(12, 94);
+            this.txtOutputFolderPath.Name = "txtOutputFolderPath";
+            this.txtOutputFolderPath.ReadOnly = true;
+            this.txtOutputFolderPath.Size = new System.Drawing.Size(457, 20);
+            this.txtOutputFolderPath.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Output folder:";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(471, 93);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 9;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.Image = global::SpotifydownWrapper.Properties.Resources.DownloadStaticEmpty;
+            this.pbStatus.Location = new System.Drawing.Point(12, 148);
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(24, 24);
+            this.pbStatus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbStatus.TabIndex = 10;
+            this.pbStatus.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 180);
+            this.Controls.Add(this.pbStatus);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOutputFolderPath);
+            this.Controls.Add(this.tsMenuBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lTracksDone);
+            this.Controls.Add(this.lStatus);
+            this.Controls.Add(this.pbProgress);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtPlaylistID);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.tsMenuBar.ResumeLayout(false);
+            this.tsMenuBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStatus)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPlaylistID;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.ProgressBar pbProgress;
+        private System.Windows.Forms.Label lStatus;
+        private System.Windows.Forms.Label lTracksDone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStrip tsMenuBar;
+        private System.Windows.Forms.ToolStripDropDownButton tsddbMenuBarHelp;
+        private System.Windows.Forms.ToolStripMenuItem tsmiMenuBarHelpAbout;
+        private System.Windows.Forms.FolderBrowserDialog fbdBrowse;
+        private System.Windows.Forms.TextBox txtOutputFolderPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.PictureBox pbStatus;
+    }
+}
+
---
+++ SpotifydownWrapper/MainForm.cs
@@ -0,0 +1,268 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SpotifydownWrapper.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpotifydownWrapper
+{
+    public partial class MainForm : Form
+    {
+        public const string API_URL = "api.spotifydown.com";
+        public const string APPLICATION_NAME = "SpotifydownWrapper";
+        public string OutputFolder;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Text = APPLICATION_NAME;
+            Icon = Resources.Logo;
+        }
+
+        public async Task<string> RequestAPIURL(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Origin", "https://spotifydown.com");
+            httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Dest", "empty");
+            httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Mode", "cors");
+            httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Site", "same-site");
+            httpClient.DefaultRequestHeaders.Add("Referer", "https://spotifydown.com/");
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0");
+            return await httpClient.GetStringAsync($"https://{API_URL}/{url}");
+        }
+
+        public async Task<byte[]> RequestAudioURL(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Origin", "https://spotifydown.com");
+            httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Dest", "empty");
+            httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Mode", "cors");
+            httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Site", "same-site");
+            httpClient.DefaultRequestHeaders.Add("Referer", "https://spotifydown.com/");
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0");
+            return await httpClient.GetByteArrayAsync(url);
+        }
+
+        public async Task<string[]> GetPlaylistTracks(string playlistID) 
+        {
+            try
+            {
+                string request = await RequestAPIURL($"trackList/playlist/{playlistID}");
+                List<string> tracks = new List<string>();
+
+                JObject parsedRequest = JsonConvert.DeserializeObject<JObject>(request);
+                JArray trackList = (JArray)parsedRequest.GetValue("trackList");
+
+                foreach (JToken token in trackList.ToArray())
+                {
+                    tracks.Add(token["id"].Value<string>());
+                }
+
+                return tracks.ToArray();
+            }
+            catch (Exception ex) 
+            {
+                await Task.Run(new Action(() =>
+                {
+                    MessageBox.Show(
+                        $"Unable to get the tracks from the playlist {playlistID}:{Environment.NewLine}" +
+                        $"{ex}",
+                        $"{APPLICATION_NAME} - Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }));
+                return new string[0];
+            }
+        }
+
+        public async Task<(string, string, string)> GetTrack(string trackID) 
+        {
+            try 
+            {
+                string request = await RequestAPIURL($"download/{trackID}");
+                JObject parsedRequest = JsonConvert.DeserializeObject<JObject>(request);
+                JToken metadata = parsedRequest.GetValue("metadata");
+                JToken downloadLink = parsedRequest.GetValue("link");
+                return (metadata["title"].Value<string>(), 
+                    metadata["artists"].Value<string>(), 
+                    downloadLink.Value<string>());
+            }
+            catch (Exception ex) 
+            {
+                await Task.Run(new Action(() =>
+                {
+                    MessageBox.Show(
+                        $"Unable to get the download link for the track {trackID}:{Environment.NewLine}" +
+                        $"{ex}",
+                        $"{APPLICATION_NAME} - Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }));
+                return (null, null, null);
+            }
+        }
+
+        public async Task DownloadTrackAudio(string trackAudioLink, string outputPath) 
+        {
+            try
+            {
+                byte[] request = await RequestAudioURL(trackAudioLink);
+                FileStream file = new FileStream(outputPath, FileMode.Create);
+                await file.WriteAsync(request, 0, request.Length);
+                await file.FlushAsync();
+                file.Close();
+            }
+            catch (Exception ex)
+            {
+                await Task.Run(new Action(() => 
+                {
+                    MessageBox.Show(
+                        $"Unable to download the audio {trackAudioLink}:{Environment.NewLine}" +
+                        $"{ex}",
+                        $"{APPLICATION_NAME} - Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }));
+            }
+        }
+
+        private void ResetUI() 
+        {
+            pbProgress.Maximum = 0;
+            pbProgress.Value = 0;
+            lStatus.Text = "Not downloading";
+            pbStatus.Image = Resources.DownloadStaticEmpty;
+            lTracksDone.Text = "0/0";
+            btnDownload.Enabled = true;
+        }
+
+        private void ResetUIWithError() 
+        {
+            ResetUI();
+            pbProgress.Maximum = 100;
+            pbProgress.Value = 100;
+            lStatus.Text = "Error";
+            pbStatus.Image = Resources.DownloadStaticError;
+        }
+
+        private async void btnDownload_Click(object sender, EventArgs e)
+        {
+            ResetUI();
+            string playlistID = txtPlaylistID.Text;
+            long startTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+
+            if (string.IsNullOrWhiteSpace(OutputFolder) || !Directory.Exists(OutputFolder))
+            {
+                await Task.Run(new Action(() =>
+                {
+                    MessageBox.Show(
+                        $"Invalid output folder! Did it get deleted or you didn't choose one?",
+                        $"{APPLICATION_NAME} - Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }));
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(playlistID)) 
+            {
+                await Task.Run(new Action(() =>
+                {
+                    MessageBox.Show(
+                        $"Invalid playlist ID!",
+                        $"{APPLICATION_NAME} - Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }));
+                return;
+            }
+
+            btnDownload.Enabled = false;
+            lStatus.Text = $"Fetching tracks from the playlist {playlistID}";
+            pbStatus.Image = Resources.DownloadAnimation;
+            string[] tracks = await GetPlaylistTracks(playlistID);
+
+            if (tracks.Length < 1) 
+            {
+                ResetUIWithError();
+                return;
+            }
+
+            pbProgress.Maximum = tracks.Length * 2 + 25;
+            lTracksDone.Text = $"0/{tracks.Length}";
+            pbProgress.Value += 25;
+
+            int tracksDone = 0;
+            foreach (string track in tracks) 
+            {
+                lStatus.Text = $"Fetching track {track}";
+                (string, string, string) trackInfo = await GetTrack(track);
+                string title = trackInfo.Item1;
+                string artist = trackInfo.Item2;
+                string downloadLink = trackInfo.Item3;
+
+                pbProgress.Value += 1;
+                if (title == null || artist == null || downloadLink == null) 
+                {
+                    pbProgress.Value += 1;
+                    continue;
+                }
+
+                if (string.IsNullOrWhiteSpace(OutputFolder) || !Directory.Exists(OutputFolder))
+                {
+                    await Task.Run(new Action(() =>
+                    {
+                        MessageBox.Show(
+                            $"The output folder no longer exists! Did it get deleted?",
+                            $"{APPLICATION_NAME} - Error",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error
+                        );
+                    }));
+                    ResetUIWithError();
+                    return;
+                }
+
+                lStatus.Text = $"Downloading track {track}";
+                if (title.Any(c => Path.GetInvalidFileNameChars().Contains(c))) title = track;
+                if (artist.Any(c => Path.GetInvalidFileNameChars().Contains(c))) artist = null;
+                await DownloadTrackAudio(downloadLink, $"{OutputFolder}\\{(artist == null ? "" : $"{artist} - ")}{title}.mp3");
+                pbProgress.Value += 1;
+
+                tracksDone += 1;
+                lTracksDone.Text = $"{tracksDone}/{tracks.Length}";
+            }
+
+            long endTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            lStatus.Text = $"Downloaded {tracksDone} tracks in {(endTime - startTime) / 1000} seconds";
+            pbStatus.Image = Resources.DownloadStaticFilled;
+            btnDownload.Enabled = true;
+        }
+
+        private void tsmiMenuBarHelpAbout_Click(object sender, EventArgs e)
+        {
+            new AboutForm().ShowDialog();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            fbdBrowse.ShowDialog();
+            OutputFolder = fbdBrowse.SelectedPath;
+            txtOutputFolderPath.Text = OutputFolder;
+        }
+    }
+}
